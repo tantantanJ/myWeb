c830e3261a87d37196c235169d83a0ba9032b0d5
Jason <cdxq@outlook.com>
2016-10-16 18:34:00 +0800

    1016-4

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,31 +1,48 @@
-;$(function(){
-	var sidebar=$('#sidebar'),
-	    mask=$('#mask'),
-	    triggerSidebar=$('#triggerSidebar'),
-	    backbutton=$('#back-to-top');
+$(function(){
+	$('#head_included').load("head.html");
 	
-	function showSidebar(){
-		mask.fadeIn();
-		sidebar.css('right',0);
-	}
-	function hideSidebar(){
-		mask.fadeOut();
-		sidebar.css('right',-sidebar.width());
-	}
-	
-	triggerSidebar.on('click',showSidebar);
-	mask.on('click',hideSidebar);
-	
-	backbutton.on('click',function(){
-		 $("html,body").animate({scrollTop:0},800);
-	})
-	
-	$(window).on('scroll',function(){
-		if($(window).scrollTop()>$(window).height()){
-			backbutton.fadeIn();
-		    }else{backbutton.fadeOut();}
-    })
-    
-    $(window).trigger('scroll')
-})
+	$(function(){
+		var sidebar=$('#sidebar'),
+		    mask=$('#mask'),
+		    triggerSidebar=$('#triggerSidebar'),
+		    backbutton=$('#back-to-top');
+		
+		function showSidebar(){
+			mask.fadeIn();
+			sidebar.css('right',0);
+		}
+		function hideSidebar(){
+			mask.fadeOut();
+			sidebar.css('right',-sidebar.width());
+		}
+		triggerSidebar.on('click',showSidebar);
+		mask.on('click',hideSidebar);
+		
+		backbutton.on('click',function(){
+			 $("html,body").animate({scrollTop:0},800);
+		})
+		
+		$(window).on('scroll',function(){
+			if($(window).scrollTop()>$(window).height()){
+				backbutton.fadeIn();
+			    }else{backbutton.fadeOut();}
+	    })
+	    $(window).trigger('scroll')
+		
+		//判断scroll向上还是向下，来触发head隐藏还是显示
+		var p=0,t=0;  
+		var nav=$('nav');
+		$(window).scroll(function(e){  
+			p = $(this).scrollTop();  
+			if(t<p){//下滚  
+				nav.css('top',-nav.height());
+			}  
+			else{//上滚  
+				nav.css('top',0);
+			}  
+			setTimeout(function(){t = p;},0);         
+		});  
+	});	
+});
+
 
